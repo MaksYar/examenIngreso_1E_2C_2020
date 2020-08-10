0269d7fc598af25395882e1848a2e1ad927e26fb
MaksYar <yaremtech@gmail.com>
2020-08-10 20:03:00 -0300

    Resolucion del ejercicio 2 y 3

--- dos.js
+++ dos.js
@@ -7,5 +7,69 @@ b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
 */function mostrar()
 {
-  alert("dos");
+  let marcaIngr;
+	let precioIngr;
+	let pesoIngr;
+	let tipoIngr;
+	let acumuladorpeso = 0;
+	let marcasolidomasbarato;
+	let preciosolidomasbarato;
+	let marcaLiquidomascaro;
+	let precioLiquidomascaro;
+	let flagprimeringresoliquido = 0;
+	let flagprimeringresosolido = 0;
+
+
+	do {
+		marcaIngr = prompt("Ingrese la marca");
+
+		do {
+			precioIngr = parseInt(prompt("Ingrese el precio"));
+		}
+		while (isNaN(precioIngr));
+		do {
+			pesoIngr = parseInt(prompt("ingrese el peso"));
+		}
+		while (isNaN(pesoIngr));
+		do {
+			tipoIngr = prompt("Ingrese si es solido o liquido");
+		}
+		while (!(tipoIngr == "solido" || tipoIngr == "liquido"));
+
+
+		acumuladorpeso += pesoIngr;
+
+		if (tipoIngr == "liquido") {
+			if (flagprimeringresoliquido == 0) {
+				flagprimeringresoliquido = 1;
+				marcaLiquidomascaro = marcaIngr;
+				precioLiquidomascaro = precioIngr;
+			}
+			else if (precioLiquidomascaro < precioIngr) {
+				marcaLiquidomascaro = marcaIngr;
+				precioLiquidomascaro = precioIngr;
+				}
+			}
+			if (tipoIngr == "solido") {
+				if (flagprimeringresosolido == 0) {
+					flagprimeringresosolido = 1;
+					preciosolidomasbarato = precioIngr;
+					marcasolidomasbarato = marcaIngr;
+				}
+				else if (preciosolidomasbarato > precioIngr) {
+					marcasolidomasbarato = marcaIngr;
+					preciosolidomasbarato = precioIngr;
+					}
+				}
+
+		do {
+			confIngr = prompt("desea seguir ingresando? (si/no)");
+		}
+		while (!(confIngr == "si" || confIngr == "no"));
+	}
+	while (confIngr == "si");
+
+	document.write("El peso total de la compra es "+ acumuladorpeso + "kilogramos <p>");
+	document.write("La marca del mas caro de los liquidos es "+ marcaLiquidomascaro + "<p>");
+	document.write("La marca del mas barato de los solidos es " + marcasolidomasbarato + "<p>");
 }
--- tres.js
+++ tres.js
@@ -11,5 +11,94 @@ c)el promedio de personas por viaje,  que viajan en invierno
 */
 function mostrar()
 {
-	alert("tres");
+	let sexotitularIngr;
+	let lugarIngr;
+	let temporadaIngr;
+	let personasIngr;
+	let confIngr;
+	let contbariloche = 0;
+	let contcataratas = 0;
+	let contsalta = 0;
+	let flagprimeraentrada = 0;
+	let sexotitular;
+	let titularmasgenerosopersonas;
+	let acumuladorviajantes=0;
+	let acumuladorviajesinvierno=0;
+	let lugarmaselegido;
+
+
+	do {
+		do {
+			sexotitularIngr = prompt("ingrese sexo del titular (m/f)");
+		}
+		while (!(sexotitularIngr == "m" || sexotitularIngr=="f"));
+
+		do {
+			lugarIngr = prompt("Ingrese destino (bariloche/cataratas/salta)");
+		}
+		while (!(lugarIngr == "bariloche" || lugarIngr == "cataratas" || lugarIngr == "salta"));
+
+		do {
+			temporadaIngr = prompt("Ingrese la temporada (otoño/invierno/verano/primavera)");
+		}
+		while (!(temporadaIngr == "primavera" || temporadaIngr == "verano" || temporadaIngr == 
+		"invierno" || temporadaIngr == "otoño"));
+		do {
+			personasIngr = parseInt(prompt("Ingrese la cantidad de personas"));
+		}
+		while (isNaN(personasIngr) && !(personasIngr > 0));
+
+		switch (lugarIngr) {
+			case "bariloche":
+				contbariloche++;
+				break;
+			case "cataratas":
+				contcataratas++;
+				break;
+			case "salta":
+				contsalta++;
+				break;
+		}
+		if (flagprimeraentrada == 0) {
+			flagprimeraentrada = 1;
+			sexotitular = sexotitularIngr;  
+			titularmasgenerosopersonas = personasIngr;
+		}
+		else if(titularmasgenerosopersonas<personasIngr){
+			sexotitular = sexotitularIngr;  
+			titularmasgenerosopersonas = personasIngr;
+		}
+		if(temporadaIngr=="invierno"){
+			acumuladorviajantes++;
+			acumuladorviajesinvierno+=personasIngr;
+
+		}
+
+		do {
+			confIngr = prompt("desea seguir ingresando? (si/no)");
+		}
+		while (!(confIngr == "si" || confIngr == "no"));
+	}
+	while (confIngr == "si");
+
+		if(contbariloche>contcataratas || contbariloche>contsalta){
+		lugarmaselegido="Bariloche";
+		}
+		else if(contbariloche<contcataratas || contcataratas>contsalta){
+		lugarmaselegido="Cataratas";
+		}
+		else if(contsalta>contcataratas || contsalta>contbariloche){
+			lugarmaselegido="Salta";
+		}
+		else{
+		lugarmaselegido="hay empate, los parquetes vedidos fueron " +contbariloche+ " para Bariloche, " + contcataratas + " para Cataratas y "+ contsalta+ " para Salta";
+		}
+
+		
+		document.write("El lugar es "+ lugarmaselegido+ "<p>");
+		document.write("El sexo del titular que lleva mas pasajeros es " + sexotitular + "<p>");
+		if(acumuladorviajantes==0){
+		acumuladorviajantes=1;
+		}
+		document.write("El promedio de personas por viaje en invierno es "+ (acumuladorviajesinvierno/acumuladorviajantes).toFixed(2));
 }
